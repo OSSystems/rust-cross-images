cc4454952dd66f698e4deb5616e7291c997b1fdc
Otavio Salvador <otavio@ossystems.com.br>
2020-09-15 22:53:20 -0300

    Add build-docker-image script
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

---
+++ build-docker-image.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -x
+set -euo pipefail
+
+version="$(git describe --always)"
+
+run() {
+  local dockerfile="Dockerfile.${1}"
+  local image_name="ossystems/rust-cross:${1}"
+  local cache_from_args=()
+
+  if ! docker image inspect "${image_name}" &>/dev/null; then
+    if docker pull "${image_name}"; then
+      cache_from_args=(--cache-from "${image_name}")
+    fi
+  fi
+
+  docker build ${cache_from_args[@]+"${cache_from_args[@]}"} --pull -t "${image_name}" -f "${dockerfile}" .
+
+  if ! [[ "${version}" =~ alpha ]] && ! [[ "${version}" =~ dev ]]; then
+    local versioned_image_name="${image_name}-${version}"
+    docker tag "${image_name}" "${versioned_image_name}"
+  fi
+}
+
+if [[ -z "${*}" ]]; then
+  for t in Dockerfile.*; do
+    run "${t##Dockerfile.}"
+  done
+else
+  for image in "${@}"; do
+    run "${image}"
+  done
+fi
